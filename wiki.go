8eccceb010431fae7dd53f4763cc5136f893e847
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-07-12 10:37:44 +0200

    Store pages in data dir

--- wiki.go
+++ wiki.go
@@ -18,12 +18,12 @@ var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func (p *Page) save() error {
-  filename := p.Title + ".txt"
+  filename := "./data/" + p.Title + ".txt"
   return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-  filename := title + ".txt"
+  filename := "./data/" + title + ".txt"
   body, err := ioutil.ReadFile(filename)
   if err != nil {
     return nil, err
