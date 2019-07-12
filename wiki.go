39f87a9509fb1980582b9bea5d86dba38949cc5f
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2019-07-12 10:11:14 +0200

    Error handling

--- wiki.go
+++ wiki.go
@@ -13,6 +13,8 @@ type Page struct {
   Body []byte
 }
 
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
 func (p *Page) save() error {
   filename := p.Title + ".txt"
   return ioutil.WriteFile(filename, p.Body, 0600)
@@ -28,8 +30,10 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p* Page) {
-  t, _ := template.ParseFiles(tmpl + ".html")
-  t.Execute(w, p)
+  err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
@@ -55,7 +59,11 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
   title := r.URL.Path[len("/save/"):]
   body := r.FormValue("body")
   p := &Page{Title: title, Body: []byte(body)}
-  p.save()
+  err := p.save()
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
   http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
